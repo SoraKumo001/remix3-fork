a40c0bdf2899e59919380717736122588427a263
SoraKumo <info@croud.jp>
2026-06-07 15:46:48 +0900

    feat: add server-side rendering handler for sample06 demo

--- demos/remix3-sample06/src/server.tsx
+++ demos/remix3-sample06/src/server.tsx
@@ -1,25 +1,41 @@
-import { renderToStream } from "@remix-run/ui/server";
-import { Layout } from "./root";
-import { RouterProvider } from "./provider/RouterProvider";
+import { renderToStream } from '@remix-run/ui/server'
+
+import css from './index.css?inline'
+import { App } from './App'
+import { RouterProvider } from './provider/RouterProvider'
 
 const handler = (url: string) => {
   const routerContext = {
     serverUrl: url,
     navigate: () => {},
-  };
+  }
 
   return new Response(
     renderToStream(
-      <RouterProvider value={routerContext}>
-        <Layout />
-      </RouterProvider>
+      <html lang="ja">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <style type="text/css">{css}</style>
+          <script
+            type="module"
+            src={/\.(tsx|ts)$/.test(import.meta.url) ? '/src/client.tsx' : '/client.js'}
+          />
+          <title>Remix3 Test</title>
+        </head>
+        <body>
+          <RouterProvider value={routerContext}>
+            <App />
+          </RouterProvider>
+        </body>
+      </html>,
     ),
     {
       headers: {
-        "Content-Type": "text/html",
+        'Content-Type': 'text/html',
       },
-    }
-  );
-};
+    },
+  )
+}
 
-export default handler;
+export default handler
